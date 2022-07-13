8f8f0f72a773974291010ca0ef8418478c723a9a
joshgun1993 <nabatovcoskun@gmail.com>
2022-07-13 20:22:56 +0400

    Just Delete One Using

--- ByJoshgunFinalRestaurantProject/ByJoshgunFinalRestaurantProject/Controllers/UsersController.cs
+++ ByJoshgunFinalRestaurantProject/ByJoshgunFinalRestaurantProject/Controllers/UsersController.cs
@@ -7,7 +7,6 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
 using System.Linq;
-using System.Net.Mail;
 using System.Threading.Tasks;
 
 namespace ByJoshgunFinalRestaurantProject.Controllers
